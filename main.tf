bbe5b82dffc6d44d23c831eb21624eba5795a8e0
Stephan Fasher <stephan.fasher@slalom.com>
2021-03-31 11:16:08 +0100

    initial upload

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-service"
+    storage_account_name = "stbuildtfstate"
+    container_name       = "resource-tag"
+    key                  = "github.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "rg-hello-azure" {
+  name     = "rg-github-actions"
+  location = "uksouth"
+}
